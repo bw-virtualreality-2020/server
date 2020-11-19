9b69c2d33e6f9610d612c920f0a9adafa6321044
Joshua Whitwell <josh.whitwell@gmail.com>
2020-11-19 14:43:28 -0500

    adds get project categories by id access method and endpoint

---
+++ routes/project_categories/project_category-helpers.js
@@ -0,0 +1,23 @@
+//imports
+const ProjectCategories = require('./project_category-model')
+
+//middlware
+async function validateProjectCategory(req, res, next) {
+    const project_id = req.params.id
+    const category_id = req.body.category_id
+    try {
+        const [projectCategory] = await ProjectCategories.getBy({ 'project_id': project_id, 'category_id': category_id })
+        if (!projectCategory) {
+            next()
+        } else {
+            res.status(400).json({ message: `Project already belongs to category ${category_id}` })
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+//exports
+module.exports = {
+    validateProjectCategory
+}
---
+++ routes/project_categories/project_category-model.js
@@ -0,0 +1,32 @@
+//imports
+const db = require('../../database/db-config')
+
+//access methods
+function getById(id) {
+    return db('project_categories as pc')
+        .join('projects as p', 'pc.project_id', 'p.project_id')
+        .join('categories as c', 'pc.category_id', 'c.category_id')
+        .select('c.category_name', 'c.category_id')
+        .where({ 'pc.project_id': id })
+}
+
+function getBy(filter) {
+    return db('project_categories').where(filter)
+}
+
+async function add(ids) {
+    try {
+        const [id] = await db('project_categories').insert(ids, 'project_category_id')
+        return id
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
+}
+
+//exports
+module.exports = {
+    getById,
+    getBy,
+    add
+}
